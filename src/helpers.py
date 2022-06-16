c3f98a25a6d572b62c36d6f6ceeb0fb019ec0c73
Alyssa Riceman <alyssariceman@gmail.com>
2022-06-16 14:45:17 -0400

    refactor code into separate modules

---
+++ src/helpers.py
@@ -0,0 +1,73 @@
+################
+##   Consts   ##
+################
+
+
+FILENAME_BANNED_CHARS = '/\\"*:<>?|\u007f'
+FILENAME_BANNED_CHAR_RANGES = (
+    (ord("\u0000"), ord("\u001f")),  # C0
+    (ord("\u0080"), ord("\u009f")),  # C1
+    (ord("\ue000"), ord("\uf8ff")),  # Private Use Area
+    (ord("\ufdd0"), ord("\ufdef")),  # Non-Characters in Arabic Presentation Forms-A
+    (ord("\ufff0"), ord("\uffff")),  # Specials
+    (ord("\U000e0000"), ord("\U000e0fff")),  # Tags and Variation Selectors Supplement
+    (ord("\U000f0000"), ord("\U000fffff")),  # Supplementary Private Use Area-A
+    (ord("\U00100000"), ord("\U0010ffff")),  # Supplementary Private Use Area-B
+)
+
+
+###################
+##   Functions   ##
+###################
+
+
+def make_filename_valid_for_epub3(filename: str) -> str:
+    filtered_filename = ""
+
+    # Ensure filename contains only allowed chars
+    for char in filename:
+        if char in FILENAME_BANNED_CHARS:
+            continue
+
+        char_allowed = True
+        for range_bottom, range_top in FILENAME_BANNED_CHAR_RANGES:
+            char_ord = ord(char)
+            if char_ord >= range_bottom and char_ord <= range_top:
+                char_allowed = False
+                break
+
+        if char_allowed:
+            filtered_filename += char
+
+    # Ensure filename doesn't end in '.'
+    while len(filtered_filename) > 0 and filtered_filename[-1] == ".":
+        filtered_filename = filtered_filename[:-1]
+
+    if len(filtered_filename) == 0:
+        raise Exception(
+            "Attempted to put file into EPUB with filename containing only invalid characters and/or periods."
+        )
+
+    # Ensure filename is of allowed length, then return
+    if len(filtered_filename.encode("utf-8")) <= 255:
+        return filtered_filename
+    else:
+        # Assumptions:
+        # * File extension exists and is <254 bytes in length
+        # * Filenames have unique numerical identifiers before the 255-byte
+        #   mark, such that truncation won't produce name collisions
+
+        split_filename = filtered_filename.split(".")
+        ext = split_filename[-1]
+        ext_bytes = len(ext.encode("utf-8"))
+        if ext_bytes > 254:
+            raise Exception(
+                "Attempted to put file into EPUB with extension longer than 254 bytes."
+            )
+
+        name_truncated = ".".join(split_filename[:-1])[:-1]
+        full_name_truncated = "%s.%s" % (name_truncated, ext)
+        while len(full_name_truncated.encode("utf-8")) > 255:
+            name_truncated = name_truncated[:-1]
+            full_name_truncated = "%s.%s" % (name_truncated, ext)
+        return full_name_truncated
---
+++ src/main.py
@@ -0,0 +1,129 @@
+import argparse
+import asyncio
+from collections import OrderedDict
+import os
+import platform
+
+import aiohttp
+import aiolimiter
+from ebooklib import epub
+from tqdm.asyncio import tqdm
+
+from .render import *
+
+# TODO:
+# * Better kobo handling
+# * Rewrite internal links
+#   Include linkbacks at the end of the thing that was linked to, eg: "This
+#   post was linked to from reply #114 of Mad investor chaos". <a>Return
+#   there</a>.
+# * Less bad covers
+# * When taking in images of format other than PNG/JPEG/GIF/SVG, convert them
+#   to one of those formats, for EPUB specification compliance
+# * Download inline images too, not just icons
+# * Fix whatever's wrong with link-rewriting right now
+
+
+################
+##   Consts   ##
+################
+
+
+COOKIE_NAME = "_glowfic_constellation_production"
+
+
+###################
+##   Functions   ##
+###################
+
+
+def get_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description="Download glowfic from the Glowfic Constellation."
+    )
+
+    parser.add_argument("url", help="glowfic post, section, or board URL")
+
+    return parser.parse_args()
+
+
+def get_cookies() -> dict[str, str]:
+    cookies = {}
+
+    if os.path.exists("cookie"):
+        with open("cookie", "r") as fin:
+            raw = fin.read()
+            (name, cookie) = raw.split("=")
+            if name != COOKIE_NAME:
+                raise ValueError(
+                    'cookie file must start with "%s=" (no quotes)' % COOKIE_NAME
+                )
+            cookies[COOKIE_NAME] = cookie.strip()
+
+    return cookies
+
+
+async def main():
+    args = get_args()
+    cookies = get_cookies()
+
+    limiter = aiolimiter.AsyncLimiter(1, 1)
+    async with aiohttp.ClientSession(
+        connector=aiohttp.TCPConnector(limit_per_host=1), cookies=cookies
+    ) as slow_session:
+        async with aiohttp.ClientSession() as fast_session:
+            spec = await get_post_urls_and_title(slow_session, limiter, args.url)
+            print("Found %i chapters" % len(spec.stamped_urls))
+
+            book = epub.EpubBook()
+            image_map = ImageMap()
+            authors = OrderedDict()
+
+            print("Downloading chapter texts")
+            chapters = await tqdm.gather(
+                *[
+                    download_chapter(
+                        slow_session, limiter, stamped_url, image_map, authors
+                    )
+                    for (i, stamped_url) in enumerate(spec.stamped_urls)
+                ]
+            )
+            chapters = list(compile_chapters(chapters))
+            for chapter in chapters:
+                for section in chapter:
+                    book.add_item(section)
+            book.set_title(spec.title)
+
+            style = epub.EpubItem(
+                uid="style",
+                file_name="style.css",
+                media_type="text/css",
+                content=stylesheet,
+            )
+            book.add_item(style)
+
+            print("Downloading images")
+            images = await download_images(fast_session, image_map)
+
+            for image in images:
+                book.add_item(image)
+
+            for author in authors.keys():
+                book.add_author(author)
+
+            book.toc = [chapter[0] for chapter in chapters]
+            book.add_item(epub.EpubNcx())
+            book.add_item(epub.EpubNav())
+
+            book.spine = ["nav"] + [
+                section for chapter in chapters for section in chapter
+            ]
+
+            out_path = "%s.epub" % spec.title
+            print("Saving book to %s" % out_path)
+            epub.write_epub(out_path, book, {})
+
+
+if platform.system() == "Windows":
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+asyncio.run(main())
